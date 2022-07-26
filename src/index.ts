7037a3e9667a076bcdb491dbf469bfc9ea620856
Thies Nieborg <thies.nieborg@student.uva.nl>
2022-07-26 21:04:08 +0200

    Hmmm redis stom

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,28 @@
 import { getCPUTemperature, getTemperatureSensor } from "./temperature";
 import Fastify from "fastify";
 import { AddressInfo } from "net";
-import cors from "@fastify/cors";
+import CORSImport from "@fastify/cors";
+
+import { Redis, RedisTimeSeries } from "redis-modules-sdk";
 
 const fastify = Fastify({ logger: true });
 const port = 3000;
 
-fastify.register(cors);
+console.log("Trying to login to fastify with url: " + process.env.BROKER_URL);
+
+fastify.register(CORSImport);
+
+const redis = new Redis(process.env.BROKER_URL || "");
 
 fastify.get("/", (req, res) => {
 	res.send({ ok: true, room: getTemperatureSensor(), pi: getCPUTemperature() });
+});
+
+fastify.get("/redis", (req, res) => {
+	const { redis } = fastify;
+
+	console.log("Heyyy", redis);
+	res.send({ hoi: true });
 });
 
 fastify.listen({ port, host: "0.0.0.0" }, (err) => {
